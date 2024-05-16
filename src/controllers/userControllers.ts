ab40f130365536aae7602914dddc99e557d2155e
NathanLins12 <nathan.lins012@gmail.com>
2024-05-16 19:27:18 -0300

    .

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -3,6 +3,8 @@ import { Request, Response, NextFunction } from "express";
 export const userControllers = {
   async create(req: Request, res: Response, next: NextFunction) {
     try {
+
+      const {name, email, password} = req.body
       return res.status(201).json({ message: "User created!" });
     } catch (error) {
       return next(error);
