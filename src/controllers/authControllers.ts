402b8ce2bb74f5ea0f643234c74cd7a81e420b0a
NathanLins12 <nathan.lins012@gmail.com>
2024-05-29 21:15:08 -0300

    add cors, cookie parser and return user id

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -8,7 +8,10 @@ export const authControllers = {
     try {
       const { email, password } = loginSchema.parse(req.body);
 
-      const token = await authServices.login({ email, password }, userRepository);
+      const { token, id } = await authServices.login(
+        { email, password },
+        userRepository
+      );
 
       res.cookie(process.env.KEY_TOKEN, token, {
         httpOnly: true,
@@ -17,7 +20,7 @@ export const authControllers = {
         maxAge: 1000 * 60 * 60 * 18, // 18h
       });
 
-      return res.status(200).json({ message: "User logged in!" });
+      return res.status(200).json({ message: "User logged in!", id });
     } catch (error) {
       return next(error);
     }
--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,17 @@ import { appErrors } from "./errors/appErrors";
 import { pageNotFound } from "./errors/pageNotFound";
 import { sqliteConnection } from "./databases/sqlite3";
 import { runMigrations } from "./databases/sqlite3/migrations";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser())
+
+const whiteLIst = ["http://localhost:5173", "http://127.0.0.1:5173"];
+app.use(cors({ origin: whiteLIst, credentials: true }));
 app.use(routes);
 
 app.use(pageNotFound);
